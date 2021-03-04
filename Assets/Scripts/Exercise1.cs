5d9057e65145bc453d16e4b9ee69e3b7c5e5a7a1
thijsgelton <thijs.gelton@hotmail.com>
2021-03-04 10:33:34 +0100

    Init

---
+++ Assets/Scripts/Exercise1.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Exercise1 : MonoBehaviour
+{
+    [SerializeField] private ComputeShader computeShader;
+    [SerializeField] private RenderTexture renderTexture, texture;
+    [SerializeField] private Texture2D colorGradient;
+    [SerializeField] private float left;
+    [SerializeField] private float right;
+    [SerializeField] private int width, iterations, bailout, height, bottom, top;
+    
+    private void Start()
+    {
+        renderTexture.enableRandomWrite = true;
+        renderTexture.Create();
+        var kernelIndex = computeShader.FindKernel("CSMain");
+        computeShader.SetFloat("_Left", left);
+        computeShader.SetFloat("_Right", right);
+        computeShader.SetFloat("_Top", top);
+        computeShader.SetFloat("_Bottom", bottom);
+        computeShader.SetInt("_Width", width);
+        computeShader.SetInt("_Height", height);
+        computeShader.SetInt("_Iterations", iterations);
+        computeShader.SetInt("_Bailout", bailout);
+        computeShader.SetTexture(kernelIndex, "Result", renderTexture);
+        computeShader.SetTexture(kernelIndex, "_MandelbrotSet", texture);
+        computeShader.SetTexture(kernelIndex, "_ColorGradient", colorGradient);
+        computeShader.Dispatch(kernelIndex, renderTexture.width / 8, renderTexture.height / 8, 1);
+    }
+ 
+    private void OnDestroy()
+    {
+        renderTexture.Release();
+    }
+    
+    private void Reset()
+    {
+        iterations = 100;
+        bailout = 2;
+        bottom = -1;
+        left = -2.5f;
+        right = 1;
+        top = 1;
+        computeShader = null;
+        renderTexture = null;
+        texture = null;
+    }
+}
